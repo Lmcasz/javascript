5512a03e2139317294e8431cac40c76a399779da
lmcasz <lucasmongou95@gmail.com>
2021-12-28 14:07:54 -0300

    nueva entrada

---
+++ main.js
@@ -0,0 +1,7 @@
+let nombre = prompt ("hey! i'm chrome. What's is your name?")
+
+alert ("hello! " + nombre + " i hope my code was fine")
+
+let edad = prompt (nombre + " how old are u?")
+
+console.log (edad)
