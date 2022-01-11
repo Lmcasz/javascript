b11306fe867a1b048aacb6fce2beb5617d038115
lmcasz <lucasmongou95@gmail.com>
2022-01-11 14:40:50 -0300

    ciclos

--- main.js
+++ main.js
@@ -1,42 +1,15 @@
-let question = prompt ("hola! puedo hacerles un truco?");
-console.log (question);
-alert ("bueno empecemos");
+//esquema de cuantas veces comer tu comida favorita al mes//
+let comida = prompt ("cual es tu comida favorita?");
+console.log (comida);
+let mes = 30;
+for (i=1; i <= mes; i++) {
+    console.log (comida)
+    console.log (i)
+    if(( i % 7 ) == 0){
+    console.log (`hoy es el dia ${i} y no se come ${comida}`)
+    }
+else{
+	console.log (`hoy es el dia ${i} y se come ${comida}`)
 
-let question1 = prompt ("elegi un numero del 1 al 10");
-console.log (question1);
-alert ("uno dificil...");
-let question2 = prompt ("ahora sumale 5");
-console.log (question2 );
-alert ("excelente!");
-let question3  = prompt ("ahora... multiplicalo por 2");
-console.log (question3);
-alert ("se te complicaron las cuentas?");
-let question4 = prompt ("a lo que quedo, restale 4");
-console.log (question4);
-alert ("no me hagas trampa eh");
-let question5 = prompt ("ahora dividilo por 2");
-console.log (question5);
-alert ("ya casi estamos");
-let question6 = prompt ("y a lo que te quedo restale el numero que pensaste");
-console.log (question6);
-alert ("listo?");
-let question7= prompt ("el resultado es 3");
-console.log (question7);
-alert ("aprobe?");
-let nota = prompt ("mi nota es..");
-if (nota == "") {
-    alert ("pongame una nota!!!")
 }
-console.log (nota);
-if (nota <= 5 ) {
-    alert ("tan mal me fue?")
-}
-if (nota <= 7 ) {
-    alert ("no le costaba nada el 10...")
-}
-if (nota >= 1000) {
-    alert ("bueno tampoco me fue tan bien...")
-}
-else {
-    alert ("gracias por el " + nota)
 }
