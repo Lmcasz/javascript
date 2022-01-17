559e2394122ed5ece372b7b219bc70ffc7fec6c0
lmcasz <lucasmongou95@gmail.com>
2022-01-17 20:59:18 -0300

    creando funcion con prompt y alert

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var dolarValor = prompt ("cuanto vale el dolar en tu pais?");
 console.log (dolarValor)
-var euroValor = prompt ("cuanto vale el euro en tu pais?");
-console.log (euroValor)
+var cambio = prompt ("cuanto queres cambiar?")
+let resultado = 0;
+  function convertir () {
+    resultado = cambio/dolarValor ;
+}
+convertir (dolarValor, cambio);
+alert("te van a dar : U$D " + resultado);
