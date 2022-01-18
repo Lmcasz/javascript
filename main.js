c43eada3fd54d650452a7b3c600ef6b63552a9b5
lmcasz <lucasmongou95@gmail.com>
2022-01-18 14:30:25 -0300

    correccion funcion

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var dolarValor = prompt ("cuanto vale el dolar en tu pais?");
 console.log (dolarValor)
 var cambio = prompt ("cuanto queres cambiar?")
 let resultado = 0;
-  function convertir () {
+  function convertir (dolarValor, cambio) {
     resultado = cambio/dolarValor ;
+    alert("te van a dar : U$D " + resultado);
 }
 convertir (dolarValor, cambio);
-alert("te van a dar : U$D " + resultado);
