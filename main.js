10f1b21aaf107ff3c4a6f2d8368e6591b5b13a1d
lmcasz <lucasmongou95@gmail.com>
2022-01-19 21:07:23 -0300

    array creado y objeto dolar

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-var dolarValor = prompt ("cuanto vale el dolar en tu pais?");
-console.log (dolarValor)
-var cambio = prompt ("cuanto queres cambiar?")
+var dolarValor = prompt("cuanto vale el dolar en tu pais?");
+
+var cambiar = prompt("cuanto queres cambiar?");
+
 let resultado = 0;
-  function convertir (dolarValor, cambio) {
-    resultado = cambio/dolarValor ;
-    alert("te van a dar : U$D " + resultado);
+function moneda (nombre, valor, pais) {
+  this.nombre=nombre;
+  this.valor=valor;
+  this.pais=pais;
 }
-convertir (dolarValor, cambio);
+var dolar = new moneda ("dolar", dolarValor, "USA");
+
+function convertir(dolarValor, cambio) {
+  resultado = cambiar / dolarValor;
+  alert("te van a dar : U$D " + resultado.toFixed(2));
+  if (resultado >= 0) {
+    console.log(dolar)
+  }
+}
+convertir(dolarValor, cambiar);
