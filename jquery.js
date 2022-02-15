931474845d90649e55440f6908e03e28b37e964e
lmcasz <lucasmongou95@gmail.com>
2022-02-15 19:29:54 -0300

    agregado de estilos y orden dentro del container

--- jquery.js
+++ jquery.js
@@ -3,14 +3,15 @@ $("#cotizador").on("click", function(){
 })
 $("#cotizador").on("click", function(){
 $(".d-inline-block").animate({
-    'margin-left': '850px'
+    'margin-left': '900px'
     },
      2000, function() {
-        $(".d-inline-block").animate({  borderSpacing: -360 }, {
+        $(".d-inline-block").animate({ borderSpacing: -2160}, {
             step: function(now,fx) {
               $(this).css('-webkit-transform','rotate('+now+'deg)'); 
               $(this).css('-moz-transform','rotate('+now+'deg)');
               $(this).css('transform','rotate('+now+'deg)');
             },
-            duration:'slow'
-        },'linear')})})
+            duration:2000
+        },'linear')})
+    })
