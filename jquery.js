b1e511b20b0e80e2ec71d0bab8c2ae2c4757235a
lmcasz <lucasmongou95@gmail.com>
2022-02-15 18:56:01 -0300

    se creo animacion concatenada con jquery

--- jquery.js
+++ jquery.js
@@ -1,3 +1,16 @@
 $("#cotizador").on("click", function(){
     $("#resultado").fadeIn(600)
 })
+$("#cotizador").on("click", function(){
+$(".d-inline-block").animate({
+    'margin-left': '850px'
+    },
+     2000, function() {
+        $(".d-inline-block").animate({  borderSpacing: -360 }, {
+            step: function(now,fx) {
+              $(this).css('-webkit-transform','rotate('+now+'deg)'); 
+              $(this).css('-moz-transform','rotate('+now+'deg)');
+              $(this).css('transform','rotate('+now+'deg)');
+            },
+            duration:'slow'
+        },'linear')})})
